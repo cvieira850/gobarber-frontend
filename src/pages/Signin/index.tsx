5816a705c8ab74c56c760d1c93369fd457b9c330
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-05-08 21:12:15 -0300

    Creating useAuth

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -10,7 +10,7 @@ import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import { Container, Content, Background } from './styles';
 
 interface SignInFormData {
@@ -21,9 +21,7 @@ interface SignInFormData {
 const Signin: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useContext(AuthContext);
-
-  console.log(user);
+  const { signIn } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
